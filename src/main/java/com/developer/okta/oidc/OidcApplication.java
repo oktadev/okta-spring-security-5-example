e452912b0f3d8145b09e852fb4f1ae82e867d1f7
Matt Raible <matt.raible@okta.com>
2017-11-29 10:27:41 -0700

    Integrate Spring Security 5

---
+++ src/main/java/com/developer/okta/oidc/OidcApplication.java
@@ -0,0 +1,12 @@
+package com.developer.okta.oidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcApplication.class, args);
+	}
+}
